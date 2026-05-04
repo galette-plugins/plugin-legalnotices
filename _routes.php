0e07e0f57a3fc8bcb0bf2e714ac8aab059578cd2
Johan Cwiklinski <johan@x-tnd.be>
2026-05-04 19:23:25 +0200

    No longer rely on global

--- _routes.php
+++ _routes.php
@@ -21,6 +21,7 @@
 
 declare(strict_types=1);
 
+use Galette\Middleware\Authenticate;
 use GaletteLegalNotices\Controllers\MainController;
 
 //Constants and classes from plugin
@@ -29,27 +30,27 @@ require_once $module['root'] . '/_config.inc.php';
 $app->get(
     '/settings',
     [MainController::class, 'settings']
-)->setName('legalnotices_settings')->add($authenticate);
+)->setName('legalnotices_settings')->add(Authenticate::class);
 
 $app->post(
     '/settings',
     [MainController::class, 'storeSettings']
-)->setName('legalnotices_store_settings')->add($authenticate);
+)->setName('legalnotices_store_settings')->add(Authenticate::class);
 
 $app->get(
     '/pages[/{lang}/{name}]',
     [MainController::class, 'listPages']
-)->setName('legalnotices_pages')->add($authenticate);
+)->setName('legalnotices_pages')->add(Authenticate::class);
 
 $app->post(
     '/pages/change',
     [MainController::class, 'changePage']
-)->setName('legalnotices_page_change')->add($authenticate);
+)->setName('legalnotices_page_change')->add(Authenticate::class);
 
 $app->post(
     '/pages',
     [MainController::class, 'editPage']
-)->setName('legalnotices_page_edit')->add($authenticate);
+)->setName('legalnotices_page_edit')->add(Authenticate::class);
 
 $app->get(
     '/{name:legal-information|terms-of-service|privacy-policy}',
